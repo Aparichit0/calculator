77bca66eff93ca89ca19627cd0fafe659286f38d
0.dharmveersingh@gmail.com <0.dharmveersingh@gmail.com>
2022-07-24 23:01:05 +0530

    added basic maths functions

---
+++ script.js
@@ -0,0 +1,16 @@
+// basic maths functions
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
