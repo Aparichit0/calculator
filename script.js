8678246837f9a060fc38dd4b3f390b913aae1bef
Lelouch vi britannia <93782780+Aparichit0@users.noreply.github.com>
2022-08-20 22:56:24 +0530

    key mapping with DOM
    added keyHandle function & map keys with dom

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const operate = {
   multiply: nextVal => currentVal *= nextVal,
   divide: nextVal => currentVal /= nextVal,
 }
+
+// key mapping with DOM
+const numpad = document.querySelectorAll(".numpad> *> button");
+numpad.forEach(e => e.addEventListener("click", keyHandle));
+
+function keyHandle(key) {
+if (key.target.type == 'button') {
+  console.log(key.target.value)
+};
+if (key.target.type == 'submit') {
+  console.log(key.target.id)
+}
+}
