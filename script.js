a06bae6a2a09c5e16478ec72e94152dba04862d6
aparichit0 <0.dharmveersingh@gmail.com>
2022-08-22 10:40:41 +0530

    Code Reformat

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let currentVal = 0; //initial value
 
 // handle basic mathematical operations
 const operate = {
-  add: nextVal => currentVal += nextVal,
-  substract: nextVal => currentVal -= nextVal,
-  multiply: nextVal => currentVal *= nextVal,
-  divide: nextVal => currentVal /= nextVal,
-}
+  add: (nextVal) => (currentVal += nextVal),
+  subtract: (nextVal) => (currentVal -= nextVal),
+  multiply: (nextVal) => (currentVal *= nextVal),
+  divide: (nextVal) => (currentVal /= nextVal),
+};
 
 // key mapping with DOM
 const numpad = document.querySelectorAll(".numpad> *> button");
 numpad.forEach(e => e.addEventListener("click", keyHandle));
 
 function keyHandle(key) {
-if (key.target.type == 'button') {
-  console.log(key.target.value)
-};
-if (key.target.type == 'submit') {
-  console.log(key.target.id)
-}
+  if (key.target.type == "button") {
+    console.log(key.target.value);
+  }
+  if (key.target.type == "submit") {
+    console.log(key.target.id);
+  }
 }
